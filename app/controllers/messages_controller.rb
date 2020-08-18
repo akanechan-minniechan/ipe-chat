611734737b182dcf3c82e598e30d9149e98931bd
akanechan-minniechan <akane.kato@ipeinc.jp>
2020-08-18 10:43:41 +0900

    第9章

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,12 @@
 class MessagesController < ApplicationController
 
     def index
+        @messages = Message.all
+        # @message = Message.new
+    end
+
+    def create
+        binding.pry
     end
 
 end
