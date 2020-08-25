6310b0f63630b64f0790e7d4737182262524f76a
akanechan-minniechan <akane.kato@ipeinc.jp>
2020-08-25 18:18:14 +0900

    第10章

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,11 +2,50 @@ class MessagesController < ApplicationController
 
     def index
         @messages = Message.all
-        # @message = Message.new
+        @message = Message.new
     end
 
     def create
-        binding.pry
+        message = Message.new(create_params)
+        if message.save
+            redirect_to root_path, notice: 'メッセージを送信しました'
+        else
+            redirect_to root_path, notice: 'メッセージの送信に失敗しました'
+        end
+    end 
+
+    def edit
+        @message = Message.find(params[:id])
     end
+
+    def update
+        @message = Message.find(params[:id])
+        if @message.update(update_params)
+            redirect_to root_path, notice: '変更しました'
+        else
+            redirect_to root_path, notice: '変更に失敗しました'
+        end
+
+    end
+
+    def destroy
+        message = Message.find(params[:id])
+          message.destroy 
+          redirect_to "/"
+         
+       end
+
+    def create_params
+    	params.require(:message).permit(:content).merge(user_id: 1)
+    end
+
+    def update_params
+        params.require(:message).permit(:content)
+    end
+
+    
+
+    private
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "messages/index", to: "messages#index"
-  
   root 'messages#index'
+
+  get "messages/index", to: "messages#index"
+  post "messages/create", to: "messages#create"
+  get "messages/:id/edit", to: "messages#edit", as: "messages_edit"
+  patch "messages/:id/update", to: "messages#update", as: "messages_update"
+  get "messages/:id/delete", to: "messages#destroy"
 end
